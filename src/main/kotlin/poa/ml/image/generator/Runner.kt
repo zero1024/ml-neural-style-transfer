2e74c70abfc93865d5863815feeb7774ef81868d
oleg1024 <o.perekhod@profeelab.net>
2021-06-02 00:05:09 +0300

    correct usage of adam updater

--- src/main/kotlin/poa/ml/image/generator/Runner.kt
+++ src/main/kotlin/poa/ml/image/generator/Runner.kt
@@ -25,10 +25,6 @@ import javax.swing.JFrame
 import javax.swing.JLabel
 import javax.swing.WindowConstants
 
-class Runner {
-
-}
-
 fun main() {
     val imageLoader = NativeImageLoader(400, 400, 3)
 
@@ -41,19 +37,19 @@ fun main() {
     contentImage[all(), point(1), all(), all()].divi(contentImage[all(), point(1), all(), all()].maxNumber())
     contentImage[all(), point(2), all(), all()].divi(contentImage[all(), point(2), all(), all()].maxNumber())
 
-    var img = contentImage.add(0.0)
-
-    val rand = Nd4j.rand(0.1, 0.9, CpuNativeRandom(), *contentImage.shape())
-    img.addi(rand)
-    img[all(), point(0), all(), all()].divi(img[all(), point(0), all(), all()].maxNumber())
-    img[all(), point(1), all(), all()].divi(img[all(), point(1), all(), all()].maxNumber())
-    img[all(), point(2), all(), all()].divi(img[all(), point(2), all(), all()].maxNumber())
-
-    //0.01719984130859375
-//    var img = Nd4j.rand(*contentImage.shape())
+//    var img = contentImage.add(0.0)
+//
+//    val rand = Nd4j.rand(0.1, 0.9, CpuNativeRandom(), *contentImage.shape())
+//    img.addi(rand)
 //    img[all(), point(0), all(), all()].divi(img[all(), point(0), all(), all()].maxNumber())
 //    img[all(), point(1), all(), all()].divi(img[all(), point(1), all(), all()].maxNumber())
 //    img[all(), point(2), all(), all()].divi(img[all(), point(2), all(), all()].maxNumber())
+
+    //0.01719984130859375
+    var img = Nd4j.rand(*contentImage.shape())
+    img[all(), point(0), all(), all()].divi(img[all(), point(0), all(), all()].maxNumber())
+    img[all(), point(1), all(), all()].divi(img[all(), point(1), all(), all()].maxNumber())
+    img[all(), point(2), all(), all()].divi(img[all(), point(2), all(), all()].maxNumber())
 
 //    var img =
 //        FileInputStream(File("/Users/oleg1024/Downloads/some")).use { SerializationUtils.deserialize(it) as INDArray }
@@ -75,11 +71,10 @@ fun main() {
         newImg[all(), point(2), all(), all()].muli(123.68 * 2)
         showImage(Transforms.relu(newImg).mul(newImg.lt(256).castTo(DataType.DOUBLE)))
 
-        for (i in 0 until 10) {
+        for (i in 0 until 100) {
             val res = vgg19.getInputGradient(img, label2D)
-//            res.negi()
             updater.applyUpdater(res, i, 0)
-            img = img.sub(updater.state["M"]!!)
+            img = img.sub(res)
         }
         FileOutputStream(File("/Users/oleg1024/Downloads/some")).use { SerializationUtils.serialize(img, it) }
     }
